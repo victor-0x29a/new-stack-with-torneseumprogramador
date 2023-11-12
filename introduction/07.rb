c8fa60d79b78ac92871b312499c78e52a6d446af
victor-0x29a <cartoonbr495@gmail.com>
2023-11-11 18:05:48 -0800

    Feat: if ternary

---
+++ introduction/07.rb
@@ -0,0 +1,15 @@
+a = 0
+a ||= 15
+
+# Basicamente se a variavel
+# estiver nula, adicione 15
+
+# =======================
+
+# IF TERNARIO
+
+a = 1
+
+b = 0 if a.nil?
+
+# Adiciona 0 se 'a' for nula
