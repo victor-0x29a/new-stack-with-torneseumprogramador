d01f2a7b1ff5bd525643318602afe87a26da332f
victor-0x29a <cartoonbr495@gmail.com>
2023-11-11 18:17:45 -0800

    Feat: more options to ternary if

--- introduction/07.rb
+++ introduction/07.rb
@@ -13,3 +13,12 @@ a = 1
 b = 0 if a.nil?
 
 # Adiciona 0 se 'a' for nula
+
+
+# ========================
+
+stdin_simulate = "s"
+
+vai_trocar = stdin_simulate.to_s.upcase.strip == "S"
+
+# Recebe um boolean
